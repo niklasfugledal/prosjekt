9d29751e1161edc707c7363abc53fe774be35764
Tommytoys <tommy_mella_larsen@hotmail.com>
2022-11-30 11:56:23 +0100

    endringer

--- Sites/Hybler/Includes/footer.php
+++ Sites/Hybler/Includes/footer.php
@@ -3,8 +3,6 @@
 <footer class="footer-container row justify-content-center py-3">
 	<div class="text-white">IS115-Prosjekt | Niklas Fugledal & Tommy Mella Larsen</div>
 </footer>
-
-<script src="<?= $server ?>js/rentAJAX.js" defer></script>
 </body>
 
 </html>
